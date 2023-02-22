0d9317ab0ec0598736f4f09a419ab42df7bc188a
Борис Бескровный <beskrovniibv@yandex.ru>
2023-02-22 10:41:38 +0500

    add cats verbose name and label

--- cats/apps.py
+++ cats/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class CatsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cats'
+    label = 'cats_apps_slug'
+    verbose_name = 'Кошечки'
